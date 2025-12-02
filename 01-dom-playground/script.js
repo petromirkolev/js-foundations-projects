b963bc018839b6ff8d261d34c45aeddd2a3bfbf5
Petromir Kolev <petromirkolev@gmail.com>
2025-12-02 10:12:23 +0200

    Feat: Finish mirrored text input

--- 01-dom-playground/script.js
+++ 01-dom-playground/script.js
@@ -1,3 +1,30 @@
+// Add input keys
 document.querySelector('input').addEventListener('keypress', (e) => {
-  document.querySelector('#input-mirror').textContent += e.key;
+  let text = document.querySelector('#input').value;
+  checkLength(text);
 });
+// Remove last string char if backspace is pressed
+document.querySelector('input').addEventListener('keyup', (e) => {
+  if (e.key === 'Backspace') {
+    let text = document.querySelector('#input').value;
+    checkLength(text);
+    isInputEmpty();
+  }
+});
+// Limit reverse input chars to specified length
+function checkLength(text) {
+  if (text.length >= 15) {
+    document.querySelector('#input-mirror').textContent = document
+      .querySelector('#input')
+      .value.substring(document.querySelector('#input').value.length - 15);
+  } else {
+    document.querySelector('#input-mirror').textContent =
+      document.querySelector('#input').value;
+  }
+}
+// Check if whole input is deleted
+function isInputEmpty() {
+  if (document.querySelector('#input').value === '') {
+    document.querySelector('#input-mirror').textContent = '';
+  }
+}
