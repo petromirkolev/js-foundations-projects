fe914ed5a7f236d0666416a3b65a991e734c36d4
Petromir Kolev <petromirkolev@gmail.com>
2025-12-05 13:06:39 +0200

    Implement Array.prototype.find() with a pure function

---
+++ 02-array-utils/src/map.js
@@ -0,0 +1,11 @@
+// Implement Array.prototype.map() with a pure function
+function mapArray(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    result.push(callback(item));
+  }
+  return result;
+}
+// Testing
+// console.log(mapArray([1, 34, 5, 6, 78, 345, 34], (x) => x + 2));
