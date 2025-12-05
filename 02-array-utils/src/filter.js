545e23b4ff6f6d633f49818b67a741b577c4ad30
Petromir Kolev <petromirkolev@gmail.com>
2025-12-05 12:46:59 +0200

    Implement pure Array.prototype.filter() function

--- 02-array-utils/src/filter.js
+++ 02-array-utils/src/filter.js
@@ -1,2 +1,14 @@
-let array = [9, 4, 5, 2, 1, 3, 6, 8, 7, 10];
-let filteredArray = [];
+// Implement Array.prototype.filter() with a pure function
+function filterArray(array, callback) {
+  const filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    if (callback(item)) {
+      filteredArray.push(item);
+    }
+  }
+  return filteredArray;
+}
+
+// Testing
+// console.log(filterArray([9, 4, 5, 2, 1, 3, 6, 8, 7, 10], (x) => x % 2 === 0));
