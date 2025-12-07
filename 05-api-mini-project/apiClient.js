9b1ad14ccf3a0d89514dd0051ffdeb0bba36bf7a
Petromir Kolev <petromirkolev@gmail.com>
2025-12-07 15:19:35 +0200

    feat: getJson module from API URL finished

--- 05-api-mini-project/apiClient.js
+++ 05-api-mini-project/apiClient.js
@@ -1,3 +1,7 @@
-function getJson() {}
+async function getJson(url, options = {}) {
+  const response = await fetch(url, (options = {}));
+  const data = await response.json();
+  return data;
+}
 
 export { getJson };
