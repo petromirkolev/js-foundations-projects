cc8b637984248662c7703b3d4514e939c70ea096
Petromir Kolev <petromirkolev@gmail.com>
2025-12-15 12:13:51 +0200

    Promise.all drill

--- 10-dom-playground-2/script.js
+++ 10-dom-playground-2/script.js
@@ -8,9 +8,16 @@
 // await delay(500);
 // console.log('done');
 
-// // Drill 2
+// // // Drill 2
 // async function fetchJson(url) {
 //   let response = await fetch(url);
 //   if (!response.ok) throw Error('Error while fetching.');
 //   return response.json();
+// }
+
+// // Drill 3
+// async function fetchAll(urls) {
+//   const promises = urls.map((url) => fetchJson(url));
+//   return Promise.all(promises);
+//   // return Promise.all(urls.map(fetchJson));
 // }
