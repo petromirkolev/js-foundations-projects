c86b828f09249e8f0ee2790895467d52c450e981
Petromir Kolev <petromirkolev@gmail.com>
2025-12-22 19:01:59 +0200

    work on calc logic

--- 12-odin-calculator/src/calculator.js
+++ 12-odin-calculator/src/calculator.js
@@ -1,66 +1,101 @@
 // Global variables
 const display = document.querySelector('#display-value');
-const numButtons = document.querySelectorAll('[data-digit]');
-const opButtons = document.querySelectorAll('[data-op]');
-const actionButtons = document.querySelectorAll('[data-action]');
+const numButton = document.querySelectorAll('[data-digit]');
+const opButton = document.querySelectorAll('[data-op]');
+const actButton = document.querySelectorAll('[data-action]');
+const clearButton = document.querySelector('[data-action="clear"]');
+const eqButton = document.querySelector('[data-action="equals"]');
+const backButton = document.querySelector('[data-action="backspace"]');
 
-// Helper functions
-function add(a, b) {
-  return a + b;
-}
-function divide(a, b) {
-  return a / b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function subtract(a, b) {
-  return a - b;
-}
-function calculate() {
-  if (calcState.action) {
-    switch (calcState.operator) {
-      case '+':
-        return add(calcState.firstNum, calcState.secondNum);
+const calcState = {
+  firstNum: 0,
+  secondNum: 0,
+  operator: null,
+  result: null,
+  whichNum: 'first',
+};
+
+// Operators
+opButton.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const operator = e.target.textContent;
+    calcState.whichNum = 'second';
+    if (calcState.secondNum === 0) {
+      display.textContent = calcState.operator = operator;
+    } else {
+      calculate();
+      calcState.secondNum = 0;
+      display.textContent = calcState.operator = operator;
+      console.log(calcState);
     }
+  });
+});
+
+// Actions
+
+// Numbers
+numButton.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const digit = e.target.dataset.digit;
+    if (isNaN(Number(digit))) return;
+    if (calcState.whichNum === 'first') {
+      if (calcState.firstNum === 0) {
+        display.textContent = calcState.firstNum = digit;
+      } else {
+        display.textContent = calcState.firstNum += digit;
+      }
+    }
+    if (calcState.whichNum === 'second') {
+      if (calcState.secondNum === 0) {
+        display.textContent = calcState.secondNum = digit;
+      } else {
+        display.textContent = calcState.secondNum += digit;
+      }
+    }
+  });
+});
+
+// Calculator operations
+function calculate() {
+  switch (calcState.operator) {
+    case '+':
+      calcState.firstNum = calcState.result =
+        Number(calcState.firstNum) + Number(calcState.secondNum);
+      break;
+    case '-':
+      calcState.firstNum = calcState.result =
+        Number(calcState.firstNum) - Number(calcState.secondNum);
+      break;
+    case '÷':
+      calcState.firstNum = calcState.result =
+        Number(calcState.firstNum) / Number(calcState.secondNum);
+      break;
+    case '×':
+      calcState.firstNum = calcState.result =
+        Number(calcState.firstNum) * Number(calcState.secondNum);
+      break;
+    default:
+      break;
   }
 }
 
-// Calculator state
-const calcState = {
-  firstNum: 0,
-  secondNum: null,
-  operator: null,
-  action: false,
-  result: 0,
-  shouldClear: false,
-};
+// Clear calculator
+clearButton.addEventListener('click', (e) => {
+  calcState.firstNum = calcState.secondNum = display.textContent = 0;
+  calcState.operator = calcState.result = null;
+  calcState.whichNum = 'first';
+});
 
-// Numbers
-numButtons.forEach((button) =>
-  button.addEventListener('click', (e) => {
-    // Clear the display from zero first
-    if (display.textContent === '0' || !Number(display.textContent))
-      display.textContent = '';
-    // Then handle calculator numbers
-    calcState.secondNum === null
-      ? (calcState.firstNum = display.textContent += e.target.textContent)
-      : (calcState.secondNum = display.textContent += e.target.textContent);
-  })
-);
+// Get result
+eqButton.addEventListener('click', (e) => {
+  calculate();
+  calcState.secondNum = 0;
+  display.textContent = calcState.result;
+});
 
-// Operators
-opButtons.forEach((button) =>
-  button.addEventListener('click', (e) => {
-    if (!calcState.action) {
-      display.textContent = calcState.operator = e.target.textContent;
-      calcState.secondNum = 0;
-      calcState.action = true;
-    } else {
-      console.log(calculate());
-    }
-  })
-);
-
-// Actions
-actionButtons.forEach((button) => button.addEventListener('click', (e) => {}));
+backButton.addEventListener('click', (e) => {
+  if (calcState.whichNum === 'first') {
+  }
+  if (calcState.whichNum === 'second') {
+  }
+});
