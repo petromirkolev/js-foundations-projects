70dc80debb2dc5caedca83c73ac29dbfca65f8ca
Petromir Kolev <petromirkolev@gmail.com>
2025-12-24 13:11:14 +0200

    feat: open/close modal; filter books by status; search books; helper functions

--- 13-odin-library/src/script.js
+++ 13-odin-library/src/script.js
@@ -5,6 +5,9 @@ const filterStatus = document.querySelector('[data-input="filter-status"]');
 const totalBooks = document.querySelector('[data-view="count-total"]');
 const totalRead = document.querySelector('[data-view="count-read"]');
 const totalUnread = document.querySelector('[data-view="count-unread"]');
+const searchBox = document.querySelector('[data-input="search"]');
+const emptyList = document.querySelector('[data-view="empty"]');
+const modal = document.querySelector('[data-view="modal"]');
 
 // Book constructor
 function Book(title, author, year, pages) {
@@ -14,95 +17,122 @@ function Book(title, author, year, pages) {
   this.year = year;
   this.pages = pages;
   this.read = false;
-  this.createdAt = Date.now();
+  this.createdAt = new Date().toDateString();
 }
 
+// Add new book manually
+
+// submitBook.addEventListener('click', (e) => {
+//   console.log(e);
+//   // const title = document.querySelector('[data-field="title"]');
+//   // const author = document.querySelector('[data-field="author"]');
+//   // const pages = document.querySelector('[data-field="pages"]');
+//   // const year = document.querySelector('[data-field="year"]');
+// });
+
+// function addNewBook() {
+//   const title = document.querySelector('[data-field="title"]');
+//   const author = document.querySelector('[data-field="author"]');
+//   const pages = document.querySelector('[data-field="pages"]');
+//   const year = document.querySelector('[data-field="year"]');
+
+//   const newBook = new Book();
+//   myLibrary.push(newBook);
+// }
+
+const asd = document.querySelector('[data-form="book"]');
+asd.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const author = document.querySelector('[data-field="author"]').value;
+  const title = document.querySelector('[data-field="title"]').value;
+  const year = document.querySelector('[data-field="year"]').value;
+  const pages = document.querySelector('[data-field="pages"]').value;
+
+  // if (year.length !== 4) alert('Invalid year!');
+
+  // modal.classList.add('hidden');
+});
+
 // Open modal
+
 document
   .querySelector('[data-action="open-add-modal"]')
   .addEventListener('click', (e) => {
-    const modal = document.querySelector('[data-view="modal"]');
     modal.classList.remove('hidden');
   });
 
 // Close modal
 document.querySelectorAll('[data-action="close-modal"]').forEach((button) => {
   button.addEventListener('click', (e) => {
-    const modal = document.querySelector('[data-view="modal"]');
     modal.classList.add('hidden');
   });
 });
 
+// Misc
 const newBook = new Book('East of eden', 'Stein', 1992, 230);
 const newBook1 = new Book('East of eden', 'Steinb', 1992, 230);
 const newBook2 = new Book('East of eden', 'Steinbe', 1992, 230);
 const newBook3 = new Book('East of eden', 'Steinbeck', 1992, 230);
-
+const newBook4 = new Book('West of eden', 'Steinbeck', 1992, 230);
+myLibrary.push(newBook, newBook1, newBook2, newBook3, newBook4);
 newBook3.read = true;
 newBook2.read = true;
-
-myLibrary.push(newBook, newBook1, newBook2, newBook3);
-// console.log(myLibrary);
+//
 
 // Display books in table
-myLibrary.forEach((book) => {
-  const tableRow = document.createElement('tr');
-  for (const key in book) {
-    const element = book[key];
-    const tableData = document.createElement('td');
-    if (key === 'id') continue;
-    tableData.textContent = element;
-    if (key === 'read') {
-      element === true
-        ? (tableData.textContent = 'Read')
-        : (tableData.textContent = 'Not read');
+function displayBooks(books) {
+  bookTable.textContent = '';
+  books.forEach((book) => {
+    const tableRow = document.createElement('tr');
+    for (const key in book) {
+      const element = book[key];
+      const tableData = document.createElement('td');
+      if (key === 'id') continue;
+      tableData.textContent = element;
+      if (key === 'read') {
+        element === true
+          ? (tableData.textContent = 'Read')
+          : (tableData.textContent = 'Not read');
+      }
+      tableRow.appendChild(tableData);
     }
-    tableRow.appendChild(tableData);
-  }
-  bookTable.appendChild(tableRow);
-});
+    bookTable.appendChild(tableRow);
+  });
+}
 
+// Filter books by status
 filterStatus.addEventListener('change', (e) => {
-  console.log(e);
-
-  for (const element of e.target.children) {
-    switch (element) {
-      case 'all':
-        console.log('all');
-        break;
-
-      case 'read':
-        console.log('read');
-        break;
-
-      case 'unread':
-        console.log('unread');
-        break;
-
-      default:
-        break;
-    }
+  switch (e.target.value) {
+    case 'all':
+      displayBooks(myLibrary);
+      break;
+    case 'read':
+      const readBooks = myLibrary.filter((book) => {
+        if (book.read) return book;
+      });
+      displayBooks(readBooks);
+      break;
+    case 'unread':
+      const unreadBooks = myLibrary.filter((book) => {
+        if (!book.read) return book;
+      });
+      displayBooks(unreadBooks);
+      break;
+    default:
+      return;
   }
 });
-// Minimum feature set
 
-// 1. Add a book (validate title/author)
-// 2. Render table rows from state
-// 3. Toggle read/unread (button per row)
-// 4. Delete a book
-// 5. Edit a book (reuse the same modal)
-// 6. Search by title/author
-// 7. Filter by read/unread
-// 8. Sort
-
-// <tr>
-//             <td>3,000</td>
-//             <td>3,000</td>
-//             <td>3,000</td>
-//             <td>3,000</td>
-//             <td>3,000</td>
-//             <td>3,000</td>
-//           </tr>
+// Manual search of books
+searchBox.addEventListener('input', (e) => {
+  const foundBooks = myLibrary.filter((book) => {
+    return (
+      book.title.toLowerCase().includes(searchBox.value.toLowerCase()) ||
+      book.author.toLowerCase().includes(searchBox.value.toLowerCase())
+    );
+  });
+  displayBooks(foundBooks);
+});
 
 // Load total/read/unread count
 function loadBookStats() {
@@ -128,3 +158,4 @@ function loadBookStats() {
 
 // Init library
 loadBookStats();
+displayBooks(myLibrary);
