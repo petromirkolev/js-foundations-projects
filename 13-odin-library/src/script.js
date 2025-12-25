84b174bd930277c0171a7755b58e9c5730a4d83a
Petromir Kolev <petromirkolev@gmail.com>
2025-12-25 09:40:26 +0200

    finish basic library logic

--- 13-odin-library/src/script.js
+++ 13-odin-library/src/script.js
@@ -2,12 +2,14 @@
 const myLibrary = [];
 const bookTable = document.querySelector('[data-view="rows"]');
 const filterStatus = document.querySelector('[data-input="filter-status"]');
+const sortStatus = document.querySelector('[data-input="sort"]');
 const totalBooks = document.querySelector('[data-view="count-total"]');
 const totalRead = document.querySelector('[data-view="count-read"]');
 const totalUnread = document.querySelector('[data-view="count-unread"]');
 const searchBox = document.querySelector('[data-input="search"]');
 const emptyList = document.querySelector('[data-view="empty"]');
 const modal = document.querySelector('[data-view="modal"]');
+const submitBook = document.querySelector('[data-form="book"]');
 
 // Book constructor
 function Book(title, author, year, pages) {
@@ -20,119 +22,153 @@ function Book(title, author, year, pages) {
   this.createdAt = new Date().toDateString();
 }
 
-// Add new book manually
-
-// submitBook.addEventListener('click', (e) => {
-//   console.log(e);
-//   // const title = document.querySelector('[data-field="title"]');
-//   // const author = document.querySelector('[data-field="author"]');
-//   // const pages = document.querySelector('[data-field="pages"]');
-//   // const year = document.querySelector('[data-field="year"]');
-// });
-
-// function addNewBook() {
-//   const title = document.querySelector('[data-field="title"]');
-//   const author = document.querySelector('[data-field="author"]');
-//   const pages = document.querySelector('[data-field="pages"]');
-//   const year = document.querySelector('[data-field="year"]');
-
-//   const newBook = new Book();
-//   myLibrary.push(newBook);
-// }
-
-const asd = document.querySelector('[data-form="book"]');
-asd.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const author = document.querySelector('[data-field="author"]').value;
-  const title = document.querySelector('[data-field="title"]').value;
-  const year = document.querySelector('[data-field="year"]').value;
-  const pages = document.querySelector('[data-field="pages"]').value;
-
-  // if (year.length !== 4) alert('Invalid year!');
-
-  // modal.classList.add('hidden');
-});
-
-// Open modal
-
-document
-  .querySelector('[data-action="open-add-modal"]')
-  .addEventListener('click', (e) => {
-    modal.classList.remove('hidden');
-  });
-
-// Close modal
-document.querySelectorAll('[data-action="close-modal"]').forEach((button) => {
-  button.addEventListener('click', (e) => {
+// Add new book
+function addNewBook() {
+  submitBook.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const author = document.querySelector('[data-field="author"]').value;
+    const title = document.querySelector('[data-field="title"]').value;
+    const year = document.querySelector('[data-field="year"]').value;
+    const pages = document.querySelector('[data-field="pages"]').value;
+    const newBook = new Book(title, author, year, pages);
+    myLibrary.push(newBook);
+    displayBooks(myLibrary);
+    loadBookStats();
     modal.classList.add('hidden');
   });
-});
+}
 
-// Misc
-const newBook = new Book('East of eden', 'Stein', 1992, 230);
-const newBook1 = new Book('East of eden', 'Steinb', 1992, 230);
-const newBook2 = new Book('East of eden', 'Steinbe', 1992, 230);
-const newBook3 = new Book('East of eden', 'Steinbeck', 1992, 230);
-const newBook4 = new Book('West of eden', 'Steinbeck', 1992, 230);
-myLibrary.push(newBook, newBook1, newBook2, newBook3, newBook4);
-newBook3.read = true;
-newBook2.read = true;
-//
+// Add book modal listeners
+function modalListeners() {
+  // Open modal
+  document
+    .querySelector('[data-action="open-add-modal"]')
+    .addEventListener('click', (e) => {
+      modal.classList.remove('hidden');
+    });
+  // Close modal
+  document.querySelectorAll('[data-action="close-modal"]').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      modal.classList.add('hidden');
+    });
+  });
+}
 
 // Display books in table
 function displayBooks(books) {
   bookTable.textContent = '';
+
   books.forEach((book) => {
     const tableRow = document.createElement('tr');
+
     for (const key in book) {
-      const element = book[key];
-      const tableData = document.createElement('td');
       if (key === 'id') continue;
-      tableData.textContent = element;
-      if (key === 'read') {
-        element === true
-          ? (tableData.textContent = 'Read')
-          : (tableData.textContent = 'Not read');
+
+      const tableData = document.createElement('td');
+      const element = book[key];
+
+      if (key === 'title') {
+        let link = document.createElement('a');
+        link.href = '#';
+        link.textContent = element;
+        link.dataset.id = book.id;
+        tableData.appendChild(link);
+      } else if (key === 'read') {
+        tableData.textContent = element ? 'Read' : 'Not read';
+      } else {
+        tableData.textContent = element;
       }
+
       tableRow.appendChild(tableData);
     }
+
     bookTable.appendChild(tableRow);
   });
 }
 
 // Filter books by status
-filterStatus.addEventListener('change', (e) => {
-  switch (e.target.value) {
-    case 'all':
-      displayBooks(myLibrary);
-      break;
-    case 'read':
-      const readBooks = myLibrary.filter((book) => {
-        if (book.read) return book;
-      });
-      displayBooks(readBooks);
-      break;
-    case 'unread':
-      const unreadBooks = myLibrary.filter((book) => {
-        if (!book.read) return book;
-      });
-      displayBooks(unreadBooks);
-      break;
-    default:
-      return;
-  }
-});
+function filterBooks() {
+  filterStatus.addEventListener('change', (e) => {
+    switch (e.target.value) {
+      case 'all':
+        displayBooks(myLibrary);
+        break;
+      case 'read':
+        const readBooks = myLibrary.filter((book) => {
+          if (book.read) return book;
+        });
+        displayBooks(readBooks);
+        break;
+      case 'unread':
+        const unreadBooks = myLibrary.filter((book) => {
+          if (!book.read) return book;
+        });
+        displayBooks(unreadBooks);
+        break;
+      default:
+        return;
+    }
+  });
+}
+
+// Sort books helper
+function sortHelper(key) {
+  return function (a, b) {
+    const valA = a[key].toLowerCase();
+    const valB = b[key].toLowerCase();
+    if (valA > valB) return 1;
+    if (valA < valB) return -1;
+    return 0;
+  };
+}
+
+// Sort books in table
+function sortBooks() {
+  sortStatus.addEventListener('change', (e) => {
+    let sorted;
+    switch (e.target.value) {
+      case 'authorAsc':
+        sorted = myLibrary.toSorted(sortHelper('author'));
+        displayBooks(sorted);
+        break;
+      case 'authorDesc':
+        sorted = myLibrary.toSorted(sortHelper('author'));
+        displayBooks(sorted.reverse());
+        break;
+      case 'titleAsc':
+        sorted = myLibrary.toSorted(sortHelper('title'));
+        displayBooks(sorted);
+        break;
+      case 'titleDesc':
+        sorted = myLibrary.toSorted(sortHelper('title'));
+        displayBooks(sorted.reverse());
+        break;
+      case 'createdAsc':
+        sorted = myLibrary.toSorted(sortHelper('createdAt'));
+        displayBooks(sorted);
+        break;
+      case 'createdDesc':
+        sorted = myLibrary.toSorted(sortHelper('createdAt'));
+        displayBooks(sorted.reverse());
+        break;
+      default:
+        break;
+    }
+  });
+}
 
 // Manual search of books
-searchBox.addEventListener('input', (e) => {
-  const foundBooks = myLibrary.filter((book) => {
-    return (
-      book.title.toLowerCase().includes(searchBox.value.toLowerCase()) ||
-      book.author.toLowerCase().includes(searchBox.value.toLowerCase())
-    );
+function manualSearch() {
+  searchBox.addEventListener('input', (e) => {
+    const foundBooks = myLibrary.filter((book) => {
+      return (
+        book.title.toLowerCase().includes(searchBox.value.toLowerCase()) ||
+        book.author.toLowerCase().includes(searchBox.value.toLowerCase())
+      );
+    });
+    displayBooks(foundBooks);
   });
-  displayBooks(foundBooks);
-});
+}
 
 // Load total/read/unread count
 function loadBookStats() {
@@ -150,12 +186,36 @@ function loadBookStats() {
   totalUnread.textContent = `Unread: ${unreadCount}`;
 }
 
-// myLibrary.forEach((book) => {
-//   let count = 0;
-//   if (book.read === false) count++;
-//   totalUnread.textContent = `Unread: ${count}`;
-// });
+// Initialize library
+function init() {
+  const newBook1 = new Book('East of eden', 'John Steinbeck', 1952, 601);
+  const newBook2 = new Book('Of mice and men', 'John Steinbeck', 1937, 107);
+  const newBook3 = new Book('1984', 'George Orwell', 1949, 326);
+  const newBook4 = new Book('To kill a mockingbird', 'Harper Lee', 1960, 333);
+  const newBook5 = new Book('The great Gatsby', 'Francis Scott', 1925, 123);
+  const newBook6 = new Book('Meditations', 'Marcus Aurelius', 161, 105);
+  const newBook7 = new Book('My favourite book', 'Petromir Kolev', 123, 321);
+
+  myLibrary.push(
+    newBook1,
+    newBook2,
+    newBook3,
+    newBook4,
+    newBook5,
+    newBook6,
+    newBook7
+  );
+  newBook3.read = true;
+  newBook2.read = true;
+
+  loadBookStats();
+  displayBooks(myLibrary);
+  addNewBook();
+  modalListeners();
+  filterBooks();
+  sortBooks();
+  manualSearch();
+}
 
 // Init library
-loadBookStats();
-displayBooks(myLibrary);
+init();
