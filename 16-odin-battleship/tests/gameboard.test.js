d59eda92052cd689ff3b6223b9bb4073e3ea9a4b
Petromir Kolev <petromirkolev@gmail.com>
2026-01-08 07:01:41 +0200

    chore: add gameboard tests

--- 16-odin-battleship/tests/gameboard.test.js
+++ 16-odin-battleship/tests/gameboard.test.js
@@ -1,17 +1,31 @@
 // import { test, expect } from './testRunner.js';
 import { createBoard } from '../core/gameboard.js';
 
+const x = 10;
+const y = 10;
+const grid = createBoard(x, y);
+
+// Total cell amount is 100
 test('Grid has x * y cells', () => {
-  const x = 10;
-  const y = 10;
-  const grid = createBoard(x, y);
   expect(grid.length).toBe(100);
 });
 
-// test('Final dashboard coordinates match y', () => {});
+// There is a cell with x=0,y=0
+test('Cell with x=0 and y=0 exists', () => {
+  const hasCell = grid.find((cell) => cell.x === 0 && cell.y === 0);
+  expect(hasCell).toEqual({ x: 0, y: 0, state: 'empty', ship: null });
+});
 
-// total cell count is 100
+// There is a cell with x=9,y=9
+test('Cell with x=0 and y=0 exists', () => {
+  const hasCell = grid.find((cell) => cell.x === 9 && cell.y === 9);
+  expect(hasCell).toEqual({ x: 9, y: 9, state: 'empty', ship: null });
+});
 
-// there is a cell with x=0,y=0 and one with x=9,y=9
-
-// all cells have state "empty" and ship null
+// All cells have state "empty" and ship null
+test('Cell with x=0 and y=0 exists', () => {
+  const hasCell = grid.find(
+    (cell) => cell.state !== 'empty' && cell.ship !== null
+  );
+  expect(hasCell).toBe(undefined);
+});
